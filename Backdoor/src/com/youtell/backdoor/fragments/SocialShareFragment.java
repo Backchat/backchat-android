60fadf9fcae5d137a4b067e58e6cbb6150bcbd9f
Lin <lin@lastquestion.org>
2013-11-19 06:44:15 +0800

    Delete unneeded files.

--- Backdoor/src/com/youtell/backdoor/fragments/SocialShareFragment.java
+++
@@ -1,6 +0,0 @@
-package com.youtell.backdoor.fragments;
-
-import android.app.Fragment;
-
-public class SocialShareFragment extends Fragment {
-}
--- Backdoor/src/com/youtell/backdoor/fragments/TextViewFragment.java
+++
@@ -1,7 +0,0 @@
-package com.youtell.backdoor.fragments;
-
-public class TextViewFragment extends CallbackFragment<TextViewFragment.Callbacks> {
-	public interface Callbacks {
-		
-	}
-}
