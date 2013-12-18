572a29332116b3dc0d7ac0e83922783a34049107
Lin <lin@lastquestion.org>
2013-12-18 14:53:51 -0800

    Add "no friends yet"

--- Backchat/src/com/youtell/backchat/fragments/NewGabFragment.java
+++ Backchat/src/com/youtell/backchat/fragments/NewGabFragment.java
@@ -1,5 +1,11 @@
 package com.youtell.backchat.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.youtell.backchat.R;
 import com.youtell.backchat.adapters.FriendListAdapter;
 import com.youtell.backchat.models.Friend;
 import com.youtell.backchat.models.User;
@@ -9,6 +15,13 @@ import com.youtell.backchat.observers.UserObserver;
 public class NewGabFragment extends ListAdapterCallbackFragment<FriendListAdapter, FriendObserver, Friend, NewGabFragment.Callbacks>
 {
 	public interface Callbacks extends ListAdapterCallbackFragment.Callbacks<Friend> {}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		View view = inflater.inflate(R.layout.fragment_new_gab, container, false);
+		return view;
+	}
 	
 	@Override
 	protected FriendListAdapter createAdapter() {
