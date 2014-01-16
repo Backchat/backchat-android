4a50c744d41360d7f2b6574e5eb47ce530aa0ff0
Lin <lin@lastquestion.org>
2014-01-16 12:20:59 -0800

    Turn off slow internet testing

--- Backchat/src/com/youtell/backchat/Settings.java
+++ Backchat/src/com/youtell/backchat/Settings.java
@@ -28,7 +28,7 @@ public class Settings {
 
 	public static final Settings productionSettings =
 			new Settings("b773675a02695460a27af2b8c2d11d39", "3509cdc42bfd782598bcbbc6907fd661", "334350433648", 
-					"api.getbackchat.com", false, false, true, true);
+					"api.getbackchat.com", false, false, true, false);
 	
 	public static final long internetDelay = 5000;
 	
